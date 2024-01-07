cfa2d333a0eee393e0dfa706b982f042058c509e
Veeten <napkfb@tutanota.com>
2024-01-07 10:02:46 +0100

    cheated charts

--- data.js
+++ data.js
@@ -34,16 +34,63 @@ const load_into_html = (data) => {
     loader_anim.style.display = 'none';
 }
 
-const myChart_1 = new Chart("myChart_1", {
-
-});
-
-
-const myChart_2 = new Chart("myChart_2", {
-
-});
-
 //fetching the data from mockaroo api only 200 requests a day
-//const rows = fetch('https://my.api.mockaroo.com/pogoda.json?key=b3f3e0f0').then(res => res.json()).then(res => {
-//    load_into_html(res);
-//})
+const rows = fetch('https://my.api.mockaroo.com/pogoda.json?key=b3f3e0f0').then(res => res.json()).then(res => {
+    load_into_html(res);
+})
+
+fetch('https://my.api.mockaroo.com/pogoda.json?key=b3f3e0f0').then(res => res.json()).then(data => {
+    // Extracting the required data for the charts
+    const cities = data.map(entry => entry.city);
+    const temperatures = data.map(entry => entry.temperature);
+    const humidities = data.map(entry => entry.humidity);
+    const windSpeedData = data.map(entry => entry.wind_speed)
+
+    // Creating the temperature chart
+    const temperatureChart = new Chart(document.getElementById('myChart_1'), {
+      type: 'bar',
+      data: {
+        labels: cities,
+        datasets: [{
+          label: 'Temperature (°C)',
+          data: temperatures,
+          backgroundColor: 'rgba(39, 255, 255, 0.78)',
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        }
+      }
+    });
+
+    // Creating the humidity chart
+    const humidityChart = new Chart(document.getElementById('myChart_2'), {
+      type: "line",
+      data: {
+        labels: cities,
+        datasets: [{
+          label: "Humidity (%)",
+          data: humidities,
+          borderColor: "rgba(0, 44, 220, 1)",
+          borderWidth: 1,
+          fill: false
+        }, {
+          label: "Wind Speed (km/h)",
+          data: windSpeedData,
+          borderColor: "rgba(255, 255, 255, 1)",
+          borderWidth: 1,
+          fill: false
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  });
